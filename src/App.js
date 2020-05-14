e6191249b2ec3df78d84678a2a1014e9ba785ac7
Carlos Diaz <cdiaz1@echsd.org>
2020-05-14 15:48:13 -0700

    React project for sample submission using axios for calling the back end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import logo from './logo.svg';
+//import logo from './logo.jpeg'
 import './App.css';
+import axios from 'axios'
+import SampleForm from './components/SampleForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001/samples'
+})
+
+
+class App extends Component{
+  // PARENT CLASS
+  // This component is in charge of rendering all the samples in a card view format
+
+    constructor() {
+        super();
+        // we create a reference to the children element
+        this.sampleRef = createRef()
+        this.state = {
+          samples : []
+        }
+        this.getSamples();
+        this.handleAddSample = this.handleAddSample.bind(this);
+        //this.getSampleId = this.getSampleId.bind(this);
+        //this.getOnEditMode = this.getOnEditMode.bind(this);
+    }
+  
+
+  getSamples = async () => {
+      let data = await api.get('/').then(({data}) => data);
+      this.setState({ samples: data})
+  }
+  
+
+  getSampleId = async(itemId) => {
+    // we call through the reference
+    this.sampleRef.current.getSampleId(itemId);
+  }
+
+
+  deleteSample = async(itemId) => {
+  
+    try {
+      console.log('ItemId: ' + itemId);
+      // the line from below describes the 
+      //let data = await api.delete('/', { params: { id: itemId } })
+      await api.delete('/' + itemId )
+      // do we need to run the getTasks function?  
+      this.getSamples();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  updateSample = async(itemId) => {
+    // this method will allow to update the sample information for the sample selected
+    console.log('Inside the updateSample');
+
+  }
+
+  handleAddSample(sample) {
+    // This function will handle the state of the application, whenever a change is being made
+    // on the state it will reflect the change 
+
+    console.log('handleAddSample');
+    this.setState({
+      samples: [...this.state.samples, sample]
+    })
+    this.getSamples();
+  }
+
+  // getOnEditMode() {
+
+  //   return this.isOnEditMode;
+
+  // }
+  
+  // begin the section where all the card views are created
+  render() {
+    // this variable will allow to iterate among all the tasks that are created
+    const samples = this.state.samples.map((sample, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4 shadow p-3 mb-3 bg-white rounded">
+            <div className="card-title text-center">
+            <button
+            className="btn btn-info" onClick={this.getSampleId.bind(this, sample.id)}>
+            {/* // className="btn btn-info"> */}
+            <i className="fa fa-pencil-square-o"></i> 
+          </button>
+                <div className="mt-4">
+                <h5>{sample.sample_name}</h5>
+                </div>
+              <span className="badge badge-pill badge- ml-2">
+                {sample.test_type}
+              </span>
+            </div>
+            <div className="card-body">
+              <p className='text-center'>{sample.sample_collected}</p>
+              <p className='text-center'>{sample.sample_type}</p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-info btn-block" onClick={this.deleteSample.bind(this, sample.id)}>
+                  <i className="fa fa-trash"></i> Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    });
+    // end
+
+    // RETURN THE COMPONENT
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            <img src={logo}  alt="logo" />
+            <span className="badge badge-pill badge-light ml-2">
+              {/* {this.state.samples.length} */}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-4 text-center">
+                {/* <img src={logo}  alt="logo" /> */}
+                {/* <img src="logo.svg" className="App-logo" alt="logo" /> */}
+              {/* <SampleForm onAddSample={this.handleAddSample}></SampleForm> */}
+              <SampleForm onAddSample={this.handleAddSample} ref={this.sampleRef}></SampleForm>
+            </div>
+            <div className="col-md-8">
+              <div className="row">
+                {samples}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SampleForm.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+
+
+const api = axios.create({
+    //baseURL: 'https://my-json-server.typicode.com/carlosdiaz/carlosdiaz_cards_project/todos'
+    baseURL: 'http://localhost:3001/samples'
+})
+
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r && 0x3) || 0x8);
+      return v.toString(16);
+    });
+  }
+  
+
+class SampleForm extends Component {
+  // This component is the sample form used for creating and editing new samples
+  constructor () {
+    super();
+    this.myRef = React.createRef();
+    this.state = {
+      sample_name: '',
+      sample_type: 'Blood',
+      sample_collected: '',
+      sample_volume: '',
+      sample_comment: '',
+      test_type: 'ID-CHECK',
+      test_comment: '',
+      update_sample : false
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.createSample = this.createSample.bind(this);
+    this.getSampleId = this.getSampleId.bind(this);
+    this.updateSample = this.updateSample.bind(this);
+
+  }
+
+  
+  handleInputChange(e) {
+    const {value, name} = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  }
+
+  createSample = async (e) => {
+    e.preventDefault();
+
+    try {
+        const id  = uuidv4();
+        console.log('create sample');
+        console.log(this.state);
+        const { sample_name, sample_type, sample_collected, sample_volume, sample_comment, test_type, test_comment } = this.state;
+        let res = await api.post('/', {id, sample_name, sample_type, sample_collected, sample_volume, sample_comment, test_type, test_comment})    
+        console.log(res);
+        // we set into the props the new value of adding a new task
+        this.props.onAddSample(this.state);
+        //console.log(this.props);
+        this.setState({
+            sample_name: '',
+            sample_type: 'Blood',
+            sample_collected: '',
+            sample_volume: '',
+            sample_comment: '',
+            test_type: 'ID-CHECK',
+            test_comment: '',
+            update_sample : false
+        });
+      } catch (err) {
+        console.log(err);
+    }
+  }
+
+  getSampleId = async (itemId) => {
+    // this function will be called from the Parent sample
+    let data = await api.get('/'+ itemId).then(({data}) => data);
+
+    console.log(data.sample_name, data.test_type);
+
+    this.setState({
+      sample_name: data.sample_name,
+      sample_type: data.sample_type,
+      sample_collected: data.sample_collected,
+      sample_volume: data.sample_volume,
+      sample_comment: data.sample_comment,
+      test_type: data.test_type,
+      test_comment: data.test_comment,
+      update_sample : true,
+      sample_uuid: data.id
+    });
+    window.scrollTo(0, this.myRef.current.offsetTop);
+    console.log('updating variable ', this.state['update_sample']);
+
+  }
+
+  updateSample = async(e) => {
+    console.log('Calling the update sample function');
+    e.preventDefault();
+    // we get the itemId from the state
+    
+    console.log(this.state.sample_uuid);
+
+    
+    try {
+      let id  = this.state.sample_uuid;
+      console.log(this.state);
+      const { sample_name, sample_type, sample_collected, sample_volume, sample_comment, test_type, test_comment } = this.state;
+      let res = await api.put('/' + id, {sample_name, sample_type, sample_collected, sample_volume, sample_comment, test_type, test_comment})    
+      console.log(res);
+      // we set into the props the new value of adding a new task
+      this.props.onAddSample(this.state);
+      //console.log(this.props);
+      this.setState({
+          sample_name: '',
+          sample_type: 'Blood',
+          sample_collected: '',
+          sample_volume: '',
+          sample_comment: '',
+          test_type: 'ID-CHECK',
+          test_comment: '',
+          update_sample : false
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="card">
+        {/* <form onSubmit={this.createSample} className="card-body"> */}
+        <form className="card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              name="sample_name"
+              className="form-control"
+              value={this.state.sample_name}
+              onChange={this.handleInputChange}
+              placeholder="Sample Name" ref={this.myRef}
+              />
+          </div>
+          <div className="form-group">
+            <select
+                name="sample_type"
+                className="form-control"
+                value={this.state.sample_type}
+                onChange={this.handleInputChange}
+              >
+              <option>Blood</option>
+              <option>CSF</option>
+              <option>Saliva</option>
+              <option>Tumor</option>
+              <option>Urine</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="sample_collected"
+              className="form-control"
+              value={this.state.sample_collected}
+              onChange={this.handleInputChange}
+              placeholder="Sample Collected"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="sample_volume"
+              className="form-control"
+              value={this.state.sample_volume}
+              onChange={this.handleInputChange}
+              placeholder="Sample Volume"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="sample_comment"
+              className="form-control"
+              value={this.state.sample_comment}
+              onChange={this.handleInputChange}
+              placeholder="Sample Comment"
+              />
+          </div>
+          <div className="form-group">
+            <select
+                name="test_type"
+                className="form-control"
+                value={this.state.test_type}
+                defaultValue={this.state.test_type}
+                onChange={this.handleInputChange}
+              >
+              <option>ID CHECK</option>
+              <option>RNA</option>
+              <option>RWGS</option>
+              <option>WGS</option>
+              <option>URWGS</option>
+            </select>
+          </div>
+
+        {(() => {
+        if (this.state['update_sample']) {
+          console.log('focus the button');
+          return (
+            <div className="btn-group">
+            <button className="btn btn-secondary pull-left mx-2" onClick={this.updateSample.bind()}>Update</button>
+            <button className="btn btn-secondary pull-right mr-2">Cancel</button>
+            </div>
+          )
+        } else  {
+          return (
+            <button type="submit" className="btn btn-info" onClick={this.createSample.bind()}>Save</button>
+          )
+        }
+      })()}
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default SampleForm;
